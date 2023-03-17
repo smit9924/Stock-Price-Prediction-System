62f3b89b0c57347dbfbbc0fd5124ba06d4796418
unknown <smit.dpatel99245@gmail.com>
2023-03-17 13:09:21 +0530

    1D 5D 1M 3M 6M 1Y graph completely working done by smit patel

--- home/views.py
+++ home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import View
 import json
 from django.http import JsonResponse
+from twelvedata import TDClient
+
+td = TDClient(apikey="d96500d09e2c454baf996a66ebc8a5ce")
 
 # Class that render index page
 class indexPage(View):
@@ -14,8 +17,31 @@ class indexPage(View):
 class tsData(View):
 
     def get(self, request):
-        print(request.GET.get('timePeriod'))
-        responese = {
-            "name":"smit"
+        symbol = ""
+        interval = request.GET.get("interval")
+        outputsize = request.GET.get("outputsize")
+        
+        ts = td.time_series(
+            symbol="AAPL",
+            interval=interval,
+            outputsize=outputsize,
+        ).as_json()
+        xLabel = []
+        yLabel = []
+
+        # Set data as xLabel and yLabel
+        # Data in ts is in desending order
+        for data in ts:
+            xLabel.append(data["datetime"])
+            yLabel.append(data["close"])
+        
+        # Reverse the list to make data appropriate
+        xLabel.reverse()
+        yLabel.reverse()
+
+        response = {
+            "xLabel":xLabel,
+            "yLabel":yLabel,
         }
-        return JsonResponse(responese)
+
+        return JsonResponse(response)
