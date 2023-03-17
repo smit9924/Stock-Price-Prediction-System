3a61137766d8be00fb8c99da29e0fd7d2d1ae1a4
unknown <smit.dpatel99245@gmail.com>
2023-03-17 10:21:21 +0530

    chart design and ajax request setup completed by smit patel

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import indexPage
+from .views import indexPage,tsData
 
 urlpatterns = [
     path('', indexPage.as_view(), name="index_page"),
+    path('tsData/', tsData.as_view(), name="time_series_data"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
 
+# Class that render index page
 class indexPage(View):
     template_name = 'home/index.html'
 
     def get(self, request):
         return render(request, self.template_name)
+
+# Class to return timeseries data for graph
+class tsData(View):
+
+    def get(self, request):
+        print(request.GET.get('timePeriod'))
+        responese = {
+            "name":"smit"
+        }
+        return JsonResponse(responese)
